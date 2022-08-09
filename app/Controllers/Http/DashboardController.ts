4c841c78cf5dfea1769950306fc5453516010142
fakhrads <fakhriadisaputra15@gmail.com>
2022-08-10 06:47:31 +0700

    update

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class DashboardController {
     public async index({ view, auth }: HttpContextContract) {
-        await auth.use('web').authenticate
+        await auth.use('web').authenticate()
         return view.render('welcome')
     }
 }
